00a4754f3917922c47fb45aaf1062e1924f0895d
Svetlana Yashchenko <svetikoduvanchik@gmail.com>
2024-04-06 00:47:05 +0300

    new

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,1 @@
-function slugify(title) {
-  const message = title.toLowerCase().split(' ');
-  const slug = message.join('-');
-  return slug;
-}
 
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
